b5cddb3cc373ae3d74112180fa9547bb71bc8401
anhanaz012 <binteabdulhameed01@gmail.com>
2025-11-30 20:22:43 +0500

    fixed deployment issue

--- src/app/(homes)/design-agency/page.tsx
+++ src/app/(homes)/design-agency/page.tsx
@@ -1,15 +1,14 @@
-import { Metadata } from "next";
-import MainWrapper from "@/components/wrapper/main-wrapper";
-import DesignAgencyWrapper from "./_components/design-agency-wrapper";
-import HeaderFour from "@/layout/header/header-four";
-import HeroFour from "@/components/hero/hero-four";
-import AwardAreaTwo from "@/components/award/award-area-2";
-import ServiceAreaFour from "@/components/services/service-area-4";
-import CapabilitiesArea from "@/components/capabilities/capabilities-area";
 import AboutThree from "@/components/about/about-three";
 import AwardAreaThree from "@/components/award/award-area-3";
-import FooterThree from "@/layout/footer/footer-three";
+import CapabilitiesArea from "@/components/capabilities/capabilities-area";
 import CustomCursor from "@/components/common/custom-cursor";
+import HeroFour from "@/components/hero/hero-four";
+import ServiceAreaFour from "@/components/services/service-area-4";
+import MainWrapper from "@/components/wrapper/main-wrapper";
+import FooterThree from "@/layout/footer/footer-three";
+import HeaderFour from "@/layout/header/header-four";
+import { Metadata } from "next";
+import DesignAgencyWrapper from "./_components/design-agency-wrapper";
 
 export const metadata: Metadata = {
   title: "Suncube-AI - Design Agency and Portfolio Next js Template",
@@ -45,21 +44,13 @@ export default function HomeDesignAgencyPage() {
             {/* Hero area end */}
 
             {/* award area start */}
-<<<<<<< HEAD
             {/* <AwardAreaTwo /> */}
-=======
-            <AwardAreaTwo />
->>>>>>> c684cb6b0ccd4cd31c1810b14f395d143d96d1dd
             {/* award area end */}
 
             {/* service area start */}
             <ServiceAreaFour />
             {/* service area end */}
-<<<<<<< HEAD
            
-=======
-
->>>>>>> c684cb6b0ccd4cd31c1810b14f395d143d96d1dd
             {/* capability area start */}
             <CapabilitiesArea />
             {/* capability area end */}
